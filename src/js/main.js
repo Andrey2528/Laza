4ec900bfb71d4f30fc3293ff022b2138f671ae79
Andrey2528 <andreys20.2016@ukr.net>
2023-04-04 21:01:07 +0300

    Add new styles

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,57 @@
 (function ($) {
     $(document).ready(function(){
         $('.multiple-items').slick({
-            centerMode: true,
-            centerPadding: '0px',
-            slidesToShow: 1,
-            arrows: true,
             dots: true,
+            infinite: true,
+            speed: 500,
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            responsive: [
+                {
+                    breakpoint: 1500,
+                    settings: {
+                        arrows: true,
+                        centerMode: true,
+                        centerPadding: '450px',
+                        slidesToShow: 1
+                    }
+                },
+                {
+                    breakpoint: 1045,
+                    settings: {
+                        arrows: false,
+                        centerMode: true,
+                        centerPadding: '-60px',
+                        slidesToShow: 1
+                    }
+                },
+                {
+                    breakpoint: 768,
+                    settings: {
+                        arrows: false,
+                        centerMode: true,
+                        centerPadding: '40px',
+                        slidesToShow: 3
+                    }
+                },
+                    {
+                    breakpoint: 600,
+                    settings: {
+                        arrows: false,
+                        centerMode: true,
+                        centerPadding: '40px',
+                        slidesToShow: 1
+                    }
+                }
+            ]
+            
+        });
+        $('.slider2').slick({
+            dots: true,
+            infinite: true,
+            speed: 500,
+            slidesToShow: 1,
+            slidesToScroll: 1,
             responsive: [
                 {
                     breakpoint: 1500,
